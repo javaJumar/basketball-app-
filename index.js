11bfbe99abe2e5efdb5ffe6cb29d96a1db47b0f7
Chong Patterson <chong@thinkful.com>
2018-04-30 15:02:35 -0700

    1st commit

---
+++ index.js
@@ -0,0 +1,183 @@
+//the function will start the quiz when you push the start quiz button
+function pushStartButton() {
+  console.log('`pushStartButton` ran');
+  $('.quiz-button').on('click', '.start-button', event => {
+     event.preventDefault(); 
+     startQuiz();
+  });
+} 
+
+//this will generate the quiz after hitting the start quiz button
+function startQuiz(){
+  $('.starting-heading').remove();
+  $('.quiz-button').remove();
+  $('.question-container').css('display', 'block');
+  $('.questionNumber').text(1);
+  renderQuestion();
+}
+
+//this will render a question to the DOM
+function renderQuestion() {
+  console.log('`renderQuestion`');
+  $('.question-container').html(createQuestion());
+  answerSubmission();
+}
+
+//create variables to keep track of the question # and score. The DATA will be retrieved from questionslist.js 
+let questionNumber = 0; 
+let score = 0; 
+
+//this function links to questionslist.js to generate all the questions to the HTML
+function createQuestion(){
+  if (questionNumber < DATA.length){
+    return (`<h1 class='ask-question'>${DATA[questionNumber].question}</h1>
+    <form role='form' class='question-form'>
+    <section role='region'>
+    <div class='answer-list'>
+      <label class='answer-set'>
+      <input type="radio" style="vertical-align: middle" name="answer-set-1" id="answer1-1" value="${DATA[questionNumber].answers[0]}" unchecked required><span class='span-answer' for="ans-1">${DATA[questionNumber].answers[0]}</span></label>
+      <br>
+      <label class='answer-set'>
+      <input type="radio" style="vertical-align: middle" name="answer-set-1" id="answer1-2" value="${DATA[questionNumber].answers[1]}" required><span class='span-answer' for="ans-2">${DATA[questionNumber].answers[1]}</span></label>
+      <br>
+      <label class='answer-set'>
+      <input type="radio" style="vertical-align: middle" name="answer-set-1" id="answer1-3" value="${DATA[questionNumber].answers[2]}" required><span class='span-answer' for="ans-3">${DATA[questionNumber].answers[2]}</span></label>
+      <br>
+      <label class='answer-set'>
+      <input type="radio" style="vertical-align: middle" name="answer-set-1" id="answer1-4" value="${DATA[questionNumber].answers[3]}" required><span class='span-answer' for="ans-4">${DATA[questionNumber].answers[3]}</span></label>
+      <br>
+    </div>
+    <div class='submit-button'>
+        <button type='submit' class='go-submit-answer'>Submit!</button>
+    </div>
+    </section>
+    </form>`);
+  } else {
+    finalResults();
+  }
+}
+
+//what happens when the user clicks the submit button and submits their answer
+function answerSubmission(){
+   console.log('`answerSubmission`'); 
+   $('form').on('submit', event => {
+    event.preventDefault(); 
+    //console.log('tipoff button executed');
+    let chosen = $('input:checked'); 
+    //console.log(chosen);
+    let selectedAnswer = chosen.val();
+   // console.log('chosen.val', selectedAnswer)
+    let realAnswer = `${DATA[questionNumber].rightAnswer}`;
+   // console.log('realAnswer', realAnswer)
+    if (selectedAnswer === realAnswer){
+      correctAnswer();
+    } else {
+      wrongAnswer(); 
+    }
+   });
+}
+
+//what happens when you get the answer correct
+function correctAnswer(){
+  scoreUp();
+  $('.ask-question').remove();
+  $('.question-list').remove();
+  $('.question-container').html(`<section role='region' class='feedback-container row .col-12'>
+     <div class='image-of-feedback'>
+      <img class='feedback-image' src='${DATA[questionNumber].icon}' alt= '${DATA[questionNumber].alt}'>
+     </div>
+     <div class='answer-response'>
+       <h3 class='answer'>Correct, 2 points!</h3>
+       <button id ='next-button'>Next Question</button>
+     </div>
+    </section>`);
+}
+
+//what happens when you get the answer wrong
+function wrongAnswer(){
+  $('.ask-question').remove();
+  $('.question-list').remove();
+     $('.question-container').html(`<section role='region' class='feedback-container'>
+     <div class='image-of-feedback'>
+      <img class='feedback-image' src='https://www.demilked.com/magazine/wp-content/uploads/2016/06/month-in-new-york-infinite-gifs-james-curran-28.gif' alt = 'Unable to intercept a pass.'>
+     </div>
+     <div class='answer-response'>
+       <h3 class='answer'>Sorry, that's incorrect. The correct answer is ${DATA[questionNumber].rightAnswer}. You'll get it next time!</h3>
+       <button id ='next-button'>Next Question</button>
+     </div>
+    </section>`);
+}
+
+//increase score by 2 points
+function scoreUp(){
+  score += 2;
+  $('.score').text(score);
+}
+
+//increase question number by +1
+function increaseQuestionNumber () {
+    questionNumber += 1; 
+    if(questionNumber < 10) {
+  $('.questionNumber').text(questionNumber+1);
+ } 
+}
+
+//this will move onto the next question after pushing the next question button
+function changeQuestion(){
+  console.log('`changeQuestion`ran');
+   $('.question-container').on('click','#next-button', event => {
+     event.preventDefault();
+     console.log('next button worked');
+     $('.feedback-container').remove();
+     increaseQuestionNumber();
+     renderQuestion();
+   }
+)}
+
+//what is presented on the browser at the end of the quiz
+function finalResults(){
+  console.log('`finalResults`ran');
+  if(score >= 16){
+    $('.ask-question').remove();
+    $('.question-list').remove();
+    $('.question-container').html(`<div class='final-container'> 
+     <div class='score-results'>
+     <h2>You scored ${score} of out a of possible 20 points.</h2>
+     </div>
+     <div>
+       <img class='results-image' src='https://78.media.tumblr.com/7edf983f4e309fb5c87cab142ee13a58/tumblr_o2eoszsXrI1riokveo1_500.gif' alt=''Cartoon Lebron James dancing.'>
+     </div>
+     <div class='results-commentary'>
+      <h3>Incredible scoring! You have been selected to the All-Star team.  Congratulations!</h3>
+     </div>
+     <div>
+       <a href='index.html'><button class='restart-button'>Restart Quiz!</button></a>
+     </div>
+    </div>`);
+  } else {
+    $('.ask-question').remove();
+    $('.question-list').remove();
+    $('.question-container').html(`<div class='final-container'> 
+     <div class='score-results'>
+     <h2>You scored ${score} of out a of possible 20 points.</h2>
+     </div>
+     <div>
+       <img class='results-image' src='http://gifimage.net/wp-content/uploads/2017/09/basketball-animated-gif-9.gif' alt='Cartoon Michael Jordan wagging tongue and dribbling'>
+     </div>
+     <div class='results-commentary'>
+      <h3>With a little more practice, you'll be an All-Star in no time. You'll need at least 16 points!</h3>
+     </div>
+     <div>
+       <a href='index.html'><button class='restart-button'>Restart Quiz!</button></a>
+     </div>
+    </div>`);
+  }
+}
+
+//callback the main functions of the quiz. How to know which functions to callback? 
+function runQuiz(){
+   pushStartButton();
+   changeQuestion();
+}
+
+$(runQuiz); 
---
+++ questionslist.js
@@ -0,0 +1,122 @@
+const DATA = [
+    {
+      question: 'Which NBA player has the highest career points per game (PPG)?',
+      answers: [
+        'Wilt Chamberlain',
+        'Michael Jordan',
+        'Kareem Abdul-Jabbar',
+        'Kobe Bryant'
+        ],
+      rightAnswer: 'Michael Jordan',
+      icon: 'https://rfathead-res.cloudinary.com/image/upload/c_scale,f_auto,h_600,q_auto,w_600/v1506455358/23-23061_nba_Michael_Jordan_Legend_Away_realbig_prod_g99m8h.png',
+      alt: 'Michael Jordan'
+    },
+    {
+      question: 'Who is credited for creating the game of basketball?',
+      answers: [
+        'John Wooden',
+        'Dean Smith',
+        'Phog Allen',
+        'James Naismith'
+        ],
+        rightAnswer: 'James Naismith',
+        icon: 'https://esllibrary.s3.amazonaws.com/uploads/flashcard/image_thumbnail_color/3669/James-Naismith_CT.png',
+        alt: 'Dr. James Naismith'
+    },
+    {
+      question: 'What player has the highest career 3-pt FG percentage?',
+      answers: [
+        'Stephen Curry',
+        'Ray Allen',
+        'Steve Kerr',
+        'Reggie Miller'
+        ],
+        rightAnswer: 'Steve Kerr',
+        icon: 'http://i64.tinypic.com/2ekra4k.png',
+        alt: 'Steve Kerr'
+    },
+    {
+      question: 'What team drafted Kobe Bryant?',
+      answers: [
+        'Charlotte Bobcats',
+        'LA Lakers',
+        'Washington Wizards',
+        'Portland Trailblazers'
+        ],
+      rightAnswer: 'Charlotte Bobcats',
+      icon: 'https://i.pinimg.com/736x/65/e1/22/65e12242ea562613383c58e5348a25dd--charlotte-hornets-sports-logos.jpg',
+      alt: 'Charlotte Bobcats NBA Logo'
+    },
+    {
+      question: 'Who is the all-time leading NCAA Division I scorer?',
+      answers: [
+        'Wilt Chamberlain',
+        'Michael Jordan',
+        'Pete Maravich',
+        'Grant Hill'
+        ],
+      rightAnswer: 'Pete Maravich',
+      icon: 'http://www.nbajersey.store/images/7-pete-maravich-player-jerseys-shop.png',
+      alt: 'Pete Maravich'
+    },
+    {
+      question: 'Which player has the most career assists in the NBA?',
+      answers: [
+        'John Stockton',
+        'Larry Bird',
+        'Magic Johnson',
+        'Jason Kidd'
+        ],
+      rightAnswer: 'John Stockton',
+      icon: 'http://www.nbajersey.store/images/1-john-stockton-player-jerseys-shop.png',
+      alt: 'John Stockton'
+    },
+    {
+      question: 'Which school has won the most NCAA men’s college basketball championships?',
+      answers: [
+      'Duke',
+      'UCLA',
+      'Kansas',
+      'Michigan State'
+      ],
+      rightAnswer: 'UCLA',
+      icon: 'https://i.pinimg.com/originals/81/89/46/8189466271a8cf1f00926168337dddd5.png',
+      alt: 'UCLA Bruin'
+    },
+    {
+      question: 'What school did NBA great, Larry Bird, play for in the NCAA?',
+      answers: [
+        'Michigan St',
+        'Indiana St',
+        'Notre Dame',
+        'Purdue'
+      ],
+      rightAnswer: 'Indiana St',
+      icon: 'http://www.francescosanto.altervista.org/bird.png',
+      alt: 'Larry Bird'
+    },
+    {
+      question: 'What player holds the record for most consecutive double-doubles in one season since the NBA/ABA merger?',
+      answers: [
+      'Lebron James',
+      'Bill Russell',
+      'Oscar Robertson',
+      'Kevin Love'
+      ],
+      rightAnswer: 'Kevin Love',
+      icon: 'http://jrhportfolio.com/CavsFanPage/klove.png',
+      alt: 'Kevin Love'
+    },
+    {
+      question: 'What team has had the most Hall of Fame players on their roster?',
+      answers: [
+        'Boston Celtics',
+        'LA Lakers',
+        'Boston Celtics',
+        'Philadelphia 76ers'
+    ],
+    rightAnswer: 'Boston Celtics',
+    icon: 'http://timothykurek.com/wp-content/uploads/2017/10/boston-celtics-logo-transparent-png-svg-vector-on-boston-celtics-background.png',
+    alt: 'Boston Celtics Logo'
+    }
+];
